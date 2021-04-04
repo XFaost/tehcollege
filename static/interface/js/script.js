80b751e37ff0bc913158036e42d649bcd3af79a0
xfaost <xfaost@gmail.com>
2021-04-04 04:50:47 +0300

    fix nav

--- static/interface/js/script.js
+++ static/interface/js/script.js
@@ -1,14 +1,12 @@
 
-$(".top_bar__nav__item__title").on('click', function (el) {      
+function toggle_nav_menu(el) {
     // Відкриває елементи навігації таким чином, щоб випадаюче меню не виходило за межі екрену
-    
-    var el = this.parentElement;
-
     var checked = !$('.top_bar__nav__item__is_open', el).prop("checked");
     $('.top_bar__nav__item__is_open', el).prop("checked", checked);
 
     if ($('.top_bar__nav__item__menu', el).length) {        
         var elm = $('.top_bar__nav__item__menu', el);
+        $(elm).removeClass('__reverse');
         var off = elm.offset();
         var l = off.left;
         var w = elm.width();
@@ -18,23 +16,27 @@ $(".top_bar__nav__item__title").on('click', function (el) {
 
         if (!isEntirelyVisible) {
             $(elm).addClass('__reverse');
-        } else {
-            $(elm).removeClass('__reverse');
         }
     }
+}
+
+$(".top_bar__nav__item__title").on('click', function (el) {          
+    
+    var el = this.parentElement;    
+    toggle_nav_menu(el);    
 });
 
-
-
-document.addEventListener('click', function(event) {
-    // Закриває меню навігації, якщо відбувся клік поза межеми цього меню
-
-    var radio = document.querySelector('input[name="nav"]:checked');
+$(document).on('click', function (el) { 
+    
+    var radio = $('input[name="nav"]:checked').get(0);
+    
     if(radio) {
-        var nav_item = radio.parentElement;       
-        if(!nav_item.contains(event.target)) {
-            radio.checked = false;
+        var nav_item = radio.parentElement;   
+           
+        if(!nav_item.contains(el.target)) {
+            toggle_nav_menu(nav_item);
         } 
-    }         
+    } 
 });
+
 
