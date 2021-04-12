fbe2a9f64b385f2a5e590da9032a60bb70e4de07
xfaost <xfaost@gmail.com>
2021-04-12 14:38:42 +0300

    fix bug with del obj in nav

--- navigation/admin.py
+++ navigation/admin.py
@@ -7,11 +7,31 @@ from navigation.services import *
 class navigation_admin(DraggableMPTTAdmin):
     search_fields = ['name', 'url', ]
     autocomplete_fields = ['parent', ]
+    actions = ['delete_queryset']
 
     def save_model(self, request, obj, form, change):
         """Після кожного оновлення навігації оновлюємо й файл, який зберігає у собі його елементи"""
         super().save_model(request, obj, form, change)
         navigation_to_json_file()
+
+    def delete_model(self, request, obj):
+        """Після кожного видалення елемента навігації оновлюємо й файл, який зберігає у собі усі елементи навігації"""
+        obj.delete()
+        navigation_to_json_file()
+
+    def get_actions(self, request):
+        """Прибираємо delete_selected, адже її не можна переоприділити. Створюємо свою функцію для видалення виділених обєктів: delete_queryset"""
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def delete_queryset(self, request, queryset):
+        """Після кожного видалення виділених елементів навігації оновлюємо й файл, який зберігає у собі усі елементи навігації"""
+        queryset.delete()
+        navigation_to_json_file()
+
+    delete_queryset.short_description = "Видалити вибрані Елементи навігації та оновити меню на сайті"
 
 
 admin.site.register(Navigation, navigation_admin)
